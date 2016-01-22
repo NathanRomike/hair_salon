b3ee3622ddfd75728275d1b3ddd6d29bb82a4c15
NathanRomike <nathanromike@gmail.com>
2016-01-22 10:02:07 -0800

    Pass unit test to confirm save get id and get name methods are functioning. Green.

--- src/main/java/Clients.java
+++ src/main/java/Clients.java
@@ -6,10 +6,45 @@ public class Clients {
   private String mName;
   private int mStylistId;
 
+  public int getId() {
+    return mId;
+  }
+
+  public String getName() {
+    return mName;
+  }
+
+  public int getStylistId() {
+    return mStylistId;
+  }
+
   public Clients (String name, int stylists_id) {
     this.mName = name;
     this.mStylistId = stylists_id;
   }
+
+  @Override
+  public boolean equals(Object otherClient) {
+    if (!(otherClient instanceof Clients)) {
+      return false;
+    } else {
+      Clients newClient = (Clients) otherClient;
+      return this.getName().equals(newClient.getName()) &&
+        this.getId() == newClient.getId();
+    }
+  }
+
+  public void save() {
+    try (Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO clients(name, stylist_id) VALUES (:name, :stylistId)";
+      this.mId = (int) con.createQuery(sql, true)
+        .addParameter("name", this.mName)
+        .addParameter("stylistId", this.mStylistId)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
 
   public static List<Clients> all() {
     try (Connection con = DB.sql2o.open()) {
