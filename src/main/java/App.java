c24b5a4a3e5a3f264c5ae0556f4f20a8fcbd623f
NathanRomike <nathanromike@gmail.com>
2016-01-22 12:12:10 -0800

    Pass integration test to confirm link from root to page for stylist is functioning. Green.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -19,12 +19,19 @@ public class App {
 
     post("/", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
-      model.put("template", "templates/index.vtl");
-
       Stylists newStylist = new Stylists(request.queryParams("newStylistInput"));
       newStylist.save();
-
       model.put("stylists", Stylists.all());
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+
+      Stylists newStylist = Stylists.find(Integer.parseInt(request.params("id")));
+      model.put("stylist", newStylist);
+      model.put("template", "templates/stylist.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
   }
