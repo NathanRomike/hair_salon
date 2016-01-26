a5716ecbcabd3e1a32835307e6c487d5da278423
NathanRomike <nathanromike@gmail.com>
2016-01-25 20:49:41 -0800

    Remove redundant integration test.

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -49,20 +49,8 @@ public class AppTest extends FluentTest {
   public void stylistLinkDispaysPageForStylist() {
     Stylists newStylist = new Stylists("Gloria");
     newStylist.save();
-    goTo("http://localhost:4567/");
+    goTo("http://localhost:4567");
     click("a", withText("Gloria"));
     assertThat(pageSource()).contains("Gloria");
-  }
-
-  @Test
-  public void newClientsAddToStylistPage() {
-    Stylists newStylist = new Stylists("Gloria");
-    newStylist.save();
-    goTo("http://localhost:4567/newclient");
-    fill("#newClientInput").with("Charlie");
-    fillSelect("#stylistSelection").withText("Gloria");
-    submit(".btn");
-    goTo("http://localhost:4567/" + newStylist.getId());
-    assertThat(pageSource()).contains("Charlie");
   }
 }
